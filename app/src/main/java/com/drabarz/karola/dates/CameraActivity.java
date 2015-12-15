4e610d2ce70d1ba2b023e014f70d0b0b399ca3a1
Szop-kradziej <karolina.drabarz@gmail.com>
2015-12-15 23:59:50 +0100

    Create new activity layout

--- app/src/main/java/com/drabarz/karola/dates/CameraActivity.java
+++ app/src/main/java/com/drabarz/karola/dates/CameraActivity.java
@@ -1,6 +1,14 @@
 package com.drabarz.karola.dates;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 public class CameraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.camera_activity);
+    }
 }
