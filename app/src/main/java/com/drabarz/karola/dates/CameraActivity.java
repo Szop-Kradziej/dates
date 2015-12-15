78c1e81ae1ff16c46e3142e28d4eb0c95f428636
Szop-kradziej <karolina.drabarz@gmail.com>
2015-12-15 23:36:04 +0100

    Start new activity

---
+++ app/src/main/java/com/drabarz/karola/dates/CameraActivity.java
@@ -0,0 +1,6 @@
+package com.drabarz.karola.dates;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class CameraActivity extends AppCompatActivity {
+}
--- app/src/main/java/com/drabarz/karola/dates/MainActivity.java
+++ app/src/main/java/com/drabarz/karola/dates/MainActivity.java
@@ -1,17 +1,32 @@
 package com.drabarz.karola.dates;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+        addStartAdventureListener();
     }
 
+    private void addStartAdventureListener() {
+        TextView startAdventureButton = (TextView) findViewById(R.id.start_adventure);
+        startAdventureButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startCameraActivity();
+            }
+        });
+    }
+
+    private void startCameraActivity() {
+        startActivity(new Intent(this, CameraActivity.class));
+    }
 }
