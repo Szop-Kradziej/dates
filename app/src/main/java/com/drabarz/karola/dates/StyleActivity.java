68456367f1c2c837e4347921edc9406c72e3d15f
Szop-kradziej <karolina.drabarz@gmail.com>
2015-12-16 01:42:55 +0100

    SetupWithViewPager

--- app/src/main/java/com/drabarz/karola/dates/StyleActivity.java
+++ app/src/main/java/com/drabarz/karola/dates/StyleActivity.java
@@ -1,6 +1,7 @@
 package com.drabarz.karola.dates;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
@@ -18,18 +19,27 @@ public class StyleActivity extends AppCompatActivity {
 
         ViewPager viewPager = (ViewPager) findViewById(R.id.view_pager);
         viewPager.setAdapter(new StylePagerAdapter());
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        tabLayout.setupWithViewPager(viewPager);
     }
 
     private class StylePagerAdapter extends PagerAdapter {
 
         int[] drawablesIds = {R.drawable.clothes, R.drawable.clothes, R.drawable.clothes, R.drawable.clothes, R.drawable.clothes};
+        String[] titles = {"First", "First", "Third", "Last", "Second"};
 
         @Override
         public Object instantiateItem(ViewGroup container, int position) {
-            ImageView view = (ImageView) LayoutInflater.from(container.getContext()).inflate(R.layout.style_page, container,false);
+            ImageView view = (ImageView) LayoutInflater.from(container.getContext()).inflate(R.layout.style_page, container, false);
             view.setImageResource(drawablesIds[position]);
             container.addView(view);
             return view;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return titles[position];
         }
 
         @Override
